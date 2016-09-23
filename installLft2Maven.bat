29d5d2ab5d148da2d92fca950cdc308b64680d34
panama69 <github@flynnshome.com>
2016-09-22 20:19:03 -0600

    removing old ones in the cache to avoid conflicts

--- installLft2Maven.bat
+++ installLft2Maven.bat
@@ -2,6 +2,7 @@ rem little script to install the lft jar files to the maven local repo
 rem assumes UFT & LFT are installed on the machine.  If they aren't you can
 rem just remove the lines down to the first call.  open cmd and cd to the location
 rem of the lft jar files and then run the lines below.
+rem This also assumes Maven is already installed.
 
 echo off
 
@@ -11,6 +12,12 @@ echo #-------------------------------------------------------#
 for /f "delims=" %%a in ('wmic product where "Name='HP Unified Functional Testing'" get InstallLocation') do (
 cd %%a )
 cd SDK\Java
+
+rmdir /S /Q %userprofile%\.m2\repository\com\hp\lft\com.hp.lft.report
+rmdir /S /Q %userprofile%\.m2\repository\com\hp\lft\com.hp.lft.sdk
+rmdir /S /Q %userprofile%\.m2\repository\com\hp\lft\com.hp.lft.unittesting
+rmdir /S /Q %userprofile%\.m2\repository\com\hp\lft\com.hp.lft.verifications
+
 call mvn install:install-file -Dfile="com.hp.lft.report.jar" -DgroupId=com.hp.lft -DartifactId=com.hp.lft.report -Dversion=12.53.0 -Dpackaging=jar
 
 call mvn install:install-file -Dfile="com.hp.lft.sdk-standalone.jar" -DgroupId=com.hp.lft -DartifactId=com.hp.lft.sdk -Dversion=12.53.0 -Dpackaging=jar
